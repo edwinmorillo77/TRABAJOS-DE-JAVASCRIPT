0d688fbefbc056243741b1b34e3da76326fe4e69
edwinmorillo77 <75079895+edwinmorillo77@users.noreply.github.com>
2021-02-26 13:08:19 -0400

    Add files via upload

---
+++ proyecto division/division.js
@@ -0,0 +1,21 @@
+var num1 = prompt("Inserte su valor por favor: ");
+
+var num2 = prompt("Inserte su segundo valor por favor: ");
+
+var resultado= num1/num2; 
+                
+
+if (num2 == 0)
+    
+{  document.write("<h3> No se puede dividir entre cero </h3>");
+
+                }
+
+         else
+         {
+
+            document.write("<h1> RESULTADO: " + resultado, "</h1>");
+
+
+         }       
+          
