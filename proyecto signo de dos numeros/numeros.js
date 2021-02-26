1650456562e3b2bc61014798809b9052f2b22cf0
edwinmorillo77 <75079895+edwinmorillo77@users.noreply.github.com>
2021-02-26 13:18:58 -0400

    Add files via upload

---
+++ proyecto signo de dos numeros/numeros.js
@@ -0,0 +1,15 @@
+var valor = prompt("Inserte su valor por favor: ");
+
+if (valor >! 0)
+    
+{  document.write("<h1> Esa cantidad es positiva </h1>");
+
+                }
+
+         else
+         {
+
+            document.write("<h1> Esa cantidad es negativa </h1>");
+
+
+         }       
