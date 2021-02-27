52f4798078ea9b07a6b08baef4bd11ec80b08707
edwinmorillo77 <75079895+edwinmorillo77@users.noreply.github.com>
2021-02-26 20:49:19 -0400

    Add files via upload

---
+++ proyecto igualdad/igualdad.js
@@ -0,0 +1,39 @@
+var valor1 = prompt("Inserte su primer valor: ");
+var valor2 = prompt("Inserte su segundo valor por favor: ");
+var valor3 = prompt("Inserte su tercer valor por favor: ");
+
+
+if (valor1 > valor2)
+    
+            {    
+                document.write ("<h1> El valor 1 fue el mas alto: " + valor1, "</h1>");
+
+            }
+
+            else if (valor1 < valor2)
+
+            {    
+               document.write ("<h1> El valor 2 fue el mas alto: " + valor2, "</h1>");
+
+           }
+
+           else if (valor1 < valor3)
+
+           {    
+              document.write ("<h1> El valor 3 fue el mas alto: " + valor3, "</h1>");
+
+          }
+           
+           else (valor1 == valor2 == valor3)
+
+            {    
+               document.write ("<h1> LOS TRES VALORES SON IGUALES !! </h1>");
+
+           }
+
+         
+
+
+
+
+        
